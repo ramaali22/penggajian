eff3b3f9c673e37dd7f1ef2cdbc7a3b056cd34f5
muhammadramadhani <rmarmaali@gmail.com>
2024-05-07 16:37:26 +0800

    first commit

---
+++ pages/404.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col">
+        <h3 class="fs-4 mb-3"> eror</h3>
+    </div>
+    <div class="col">
+        <a href="javascript:history.back()" class="btn btn-primary float-end">
+            <i class="fab fa-arrow-circle-left"></i>>
+        </a>
+    </div>
+</div>
+<div class="row mt-3">
+    <div class="col">
+        <div class="alert alert-light" role="alert">
+            halaman tidak di temukan
+        </div>
+    </div>
+</div>
