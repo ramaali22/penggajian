f8ebb0fdbaa5eee8a47752b188ca224cd258c3c9
muhammadramadhani <rmarmaali@gmail.com>
2024-06-04 16:23:41 +0800

    update sesi-4 dan sesi-3

--- pages/karyawan/karyawanubah.php
+++ pages/karyawan/karyawanubah.php
@@ -1,10 +1,10 @@
-<div id="top" class="row mb-3">
+<div id="top" class="row mb-3 col">
     <div class="col">
         <h3>Ubah Data Karyawan</h3>
     </div>
     <div class="col">
         <a href="?page=karyawan" class="btn btn-primary float-end">
-            <i class="fa fa-arrow-circle-left"></i>
+            <i class="fa fa-arrow-cicle-left"></i>
             Kembali
         </a>
     </div>
@@ -21,42 +21,42 @@
             $gaji_pokok = $_POST['gaji_pokok'];
             $status_karyawan = $_POST['status_karyawan'];
             $bagian_id = $_POST['bagian_id'];
-
-            $updateSQL = "UPDATE karyawan SET
-                nama='$nama',
+        
+            $updateSQL ="UPDATE karyawan SET 
+                nama = '$nama',
                 tanggal_mulai='$tanggal_mulai',
-                gaji_pokok='$gaji_pokok',
+                gaji_pokok=$gaji_pokok,
                 status_karyawan='$status_karyawan',
-                bagian_id=$bagian_id
-                WHERE nik='$nik'";
-            $result = mysqli_query($connection, $checkSQL);
-            if ($result) {
-        ?>
-                <div class="alert alert-danger" role="alert">
-                    <i class="fa fa-exclamation-circle"></i>
-                    <?php echo mysqli_error($connection) ?>
-                </div>
+                bagian_id=$bagian_id 
+                WHERE nik = '$nik'";
+            $result = mysqli_query($connection, $updateSQL);
+            if (!$result) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <i class="fa fa-exclamation-circle"></i>
+                        <?php echo mysqli_error($connection) ?>
+                    </div>
+                <?php        
+                } else {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <i class="fa fa-check-circle"></i>
+                        Ubah Data Berhasil Disimpan
+                    </div>
             <?php
-            } else {
-            ?>
-                <div class="alert alert-danger" role="alert">
-                    <i class="fa fa-exclamation-circle"></i>
-                    Ubah data berhasil disimpan
-                </div>
-        <?php
-                }
+                }    
             }
-
+        
         $nik = $_GET['nik'];
         $selectSQL = "SELECT * FROM karyawan WHERE nik = $nik";
         $result = mysqli_query($connection, $selectSQL);
         if (!$result || mysqli_num_rows($result) == 0) {
-            echo '<meta http-equiv="refresh" content="0;url=?page=karyawan">';
+            echo '<meta http-equiv="refresh" content="0;url?page=karyawan">';
         }
         $row = mysqli_fetch_assoc($result);
-        $tetap_checked = $row["status_karyawan"] == "TETAP" ? " checked" : "";
-        $kontrak_checked = $row["status_karyawan"] == "KONTRAK" ? " checked" : "";
-        $magang_checked = $row["status_karyawan"] == "MAGANG" ? " checked" : "";
+        $tetap_checked = $row['status_karyawan'] == "TETAP" ? " checked" : "";
+        $kontrak_checked = $row['status_karyawan'] == "KONTRAK" ? " checked" : "";
+        $magang_checked = $row['status_karyawan'] == "MAGANG" ? " checked" : "";
         ?>
     </div>
 </div>
@@ -64,78 +64,87 @@
     <div class="col">
         <div class="card px-3 py-3">
             <form action="" method="post">
-            <div class="mb-3">
-                    <label for="nik" class="form-label">NIK</label>
-                    <input type="text" class="form-control" id="nik" name="nik" placeholder="misal : 0001" required>
-                </div>
-                <div class="mb-3">
-                    <label for="nama" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="nama" name="nama" placeholder="misal : Fulan" required>
+            <div class="mb-3 mt-3">
+                  <label for="nik" class="form-label">NIK</label>
+                  <input type="text" class="form-control" id="nik" name="nik"
+                  value="<?php echo $nik ?>" readonly>
                 </div>
                 <div class="mb-3 mt-3">
-                    <label for="tanggal_mulai" class="form-label">Tanggal Mulai Bekerja</label>
-                    <input type="date" class="form-control" id="tanggal_mulai" name="tanggal_mulai" required>
+                  <label for="nama" class="form-label">Nama</label>
+                  <input type="text" class="form-control" id="nama" name="nama" placeholder="misal: Fulan"
+                  value="<?php echo $row['nama'] ?>" required>
                 </div>
                 <div class="mb-3 mt-3">
-                    <label for="gaji_pokok" class="form-label">Gaji Pokok</label>
-                    <input type="number" class="form-control" id="gaji_pokok" name="gaji_pokok" required>
+                  <label for="tanggal_mulai" class="form-label">Tanggal Mulai Bekerja</label>
+                  <input type="date" class="form-control" id="tanggal_mulai" name="tanggal_mulai"
+                  value="<?php echo $row['tanggal_mulai'] ?>" required>
+                </div>
+                <div class="mb-3 mt-3">
+                  <label for="gaji_pokok" class="form-label">Gaji Pokok</label>
+                  <input type="number" class="form-control" id="gaji_pokok" name="gaji_pokok"
+                  value="<?php echo $row['gaji_pokok'] ?>" required>
                 </div>
                 <label class="form-label">Status Karyawan</label>
-                <div class="mb-3 mt-3">
-                    <input class="form-check-input" type="radio" name="status_karyawan" id="TETAP" value="TETAP" <?php echo $tetap_checked ?> required>
+                <div class="mb3 mt-3">
+                    <input class="form-check-input" type="radio" name="status_karyawan" id="TETAP"
+                    value="TETAP" <?php echo $tetap_checked ?> required>
                     <label class="form-check-label" for="TETAP">
-                        Tetap
+                        TETAP
                     </label>
                 </div>
-                <div class="mb-3 mt-3">
-                    <input class="form-check-input" type="radio" name="status_karyawan" id="KONTRAK" value="KONTRAK" <?php echo $kontrak_checked ?> required>
+                <div class="mb3 mt-3">
+                    <input class="form-check-input" type="radio" name="status_karyawan" id="KONTRAK"
+                    value="KONTRAK" <?php echo $kontrak_checked ?> required>
                     <label class="form-check-label" for="KONTRAK">
-                        Kontrak
+                        KONTRAK
                     </label>
                 </div>
-                <div class="mb-3 mt-3">
-                    <input class="form-check-input" type="radio" name="status_karyawan" id="MAGANG" value="MAGANG" <?php echo $magang_checked ?> required>
+                <div class="mb3 mt-3">
+                    <input class="form-check-input" type="radio" name="status_karyawan" id="MAGANG"
+                    value="MAGANG" <?php echo $magang_checked ?> required>
                     <label class="form-check-label" for="MAGANG">
-                        Magang
+                        MAGANG
                     </label>
                 </div>
-                <div class="mb-3 mt-3">
-                    <label for="bagian_id" class="form-label">Bagian</label>
-                    <?php
-                    $selectSQL = "SELECT * FROM bagian";
-                    $result = mysqli_query($connection, $selectSQL);
-                    if (!$result) {
-                    ?>
-                        <div class="alert alert-danger" role="alert">
-                            <?php echo mysqli_error($connection) ?>
-                        </div>
-                    <?php
-                        return;
-                    }
-                    if (mysqli_num_rows($result) ==0) {
-                    ?>
-                        <div class="alert alert-light" role="alert">
-                            Data Kosong
-                        </div>
-                    <?php
-                        return;
-                    }
-                    ?>
-                    <select class="form-select" aria-label="Default select example" name="bagian_id">
-                        <option value="" selected> -- Pilih Bagian -- </option>
-                        <?php
-                        while ($row_bagian = mysqli_fetch_assoc($result_bagian)) {
-                            $bagian_selected = $row["bagian_id"] ? " selected" : "";
-                        ?>
-                            <option value="<?php echo $row_bagian["id"] ?>"><?php echo $row_bagian["nama"] ?></option>
-                        <?php
-                        }
-                        ?>
-                    </select>
+        <div class="row mt-3">
+            <label for="bagian_id" class="form-label">Bagian</label>
+            <?php
+            $selectSQLBagian = "SELECT * FROM bagian";
+            $result_bagian = mysqli_query($connection, $selectSQLBagian);
+            if (!$result_bagian) {
+            ?>
+                <div class="alert alert-danger" role="alert">
+                     <?php echo mysqli_error($connection) ?>
                 </div>
-                <div class="col mb-3">
+            <?php
+                return;
+            }
+            if (mysqli_num_rows($result) == 0){
+            ?>
+                <div class="alert alert-light" role="alert">
+                    Data Kosong
+                </div>
+            <?php
+                return;
+            }
+            ?>
+            <select class="form-select" aria-label="Default select example" name="bagian_id">
+                <option value="" selected> -- Pilih Bagian -- </option>
+                <?php
+                while ($row_bagian = mysqli_fetch_array($result_bagian)) {
+                    $bagian_selected = $row["bagian_id"] == $row_bagian["id"] ? "selected":"";
+                ?>
+                    <option value="<?php echo $row_bagian["id"] ?>" <?php echo $bagian_selected ?>>
+                        <?php echo $row_bagian["nama"] ?>
+                    </option>
+                <?php
+                    }
+                    ?>
+                </select>
+                </div>
+                <div class="col mb-3 mt-3">
                     <button class="btn btn-success" type="submit" name="simpan_button">
-                        <i class="fas fa-save"></i> 
+                        <i class="fas fa-save"></i>
                         Simpan
                     </button>
                 </div>
@@ -144,7 +153,7 @@
     </div>
 </div>
 <script>
-    if (window.history.replacestate) {
+    if (window.history.replaceState){
         window.history.replaceState(null, null, window.location.href);
     }
 </script>
--- pages/penggajian/penggajianhapus.php
+++ pages/penggajian/penggajianhapus.php
@@ -1,37 +1,40 @@
+<?php
+$id = $_GET['id'];
+$bulan = $_GET['bulan'];
+$tahun = $_GET['tahun'];
+?>
 <div id="top" class="row mb-3">
     <div class="col">
-        <h3>Hapus Data Karyawan</h3>
+        <h3>Hapus Data Penggajian</h3>
     </div>
     <div class="col">
-        <a href="?page=karyawan" class="btn btn-primary float-end">
+        <a href="?page=penggajian&hapus=<?= $bulan ?>&tahun=<?= $tahun ?>" class="btn btn-primary float-end">
             <i class="fa fa-arrow-circle-left"></i>
             Kembali
         </a>
     </div>
 </div>
-<div id="pesan" class="row mb-3">
+<div id="content" class="row mb-3">
     <div class="col">
         <?php
         include "database/connection.php";
-        $nik = $_GET['nik'];
 
-        $sql = "DELETE FROM karyawan WHERE nik = $nik";
-
+        $sql = "DELETE FROM penggajian WHERE id = $id";
         $result = mysqli_query($connection, $sql);
         if (!$result) {
         ?>
             <div class="alert alert-danger" role="alert">
                 <i class="fa fa-exclamation-circle"></i>
                 <?php echo mysqli_error($connection) ?>
-            </div>
-        <?php
+            </div>  
+        <?php  
         } else {
         ?>
             <div class="alert alert-success" role="alert">
                 <i class="fa fa-check-circle"></i>
                 Hapus data berhasil
             </div>
-            <meta http-equiv="refresh" content="2;url=?page=karyawan">
+            <meta http-equiv="refresh" content="2;url=?page=penggajian&bulan=<?= $bulan ?>&tahun=<?= $tahun ?>">
         <?php
         }
         ?>
